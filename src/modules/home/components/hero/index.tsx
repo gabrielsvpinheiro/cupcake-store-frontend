570e2b411cdcd1a18a7cb89cc1f0b011419864cb
Gabriel Pinheiro <gabriel.silva.pinheiro2@gmail.com>
2024-11-30 20:33:27 -0300

    feat: edit hero

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,31 +1,31 @@
-import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+    <div
+      className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle"
+      style={{ backgroundColor: '#FFC0CB' }}
+    >
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
             level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
+            className="text-3xl leading-10 text-pink-700 font-normal"
           >
-            Ecommerce Starter Template
+            Cupcake Store
           </Heading>
           <Heading
             level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
+            className="text-3xl leading-10 text-white font-normal"
           >
-            Powered by Medusa and Next.js
+            The best cupcakes in town
           </Heading>
         </span>
         <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
+          href="/store"
         >
           <Button variant="secondary">
-            View on GitHub
-            <Github />
+            Shop now
           </Button>
         </a>
       </div>
